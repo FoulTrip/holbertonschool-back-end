9072527dcdc65a29c32fec4d5520eb55f174247b
foultrip <davidvasquezmahecha@gmail.com>
2024-05-02 11:49:24 -0500

    act002

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 """
-script that, 
-using this REST API, 
-for a given employee ID, 
+script that,
+using this REST API,
+for a given employee ID,
 returns information about his/her TODO list progress.
 """
 
