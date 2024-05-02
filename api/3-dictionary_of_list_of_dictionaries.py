b0e493509afd786da804c4a13392db2179ba5aaf
foultrip <davidvasquezmahecha@gmail.com>
2024-05-02 13:41:59 -0500

    act003

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -5,27 +5,29 @@ Script to export data in the JSON format.
 
 
 if __name__ == "__main__":
+
     import requests
+    from sys import argv
     import json
-    
+
     link = "https://jsonplaceholder.typicode.com"
 
-    users = requests.get(f"{link}/users")
-    users = users.json()
+    if len(argv) < 2:
+        exit()
+    user_id = argv[1]
+    todos = requests.get(f"{link}/todos?userId={user_id}")
+    user_info = requests.get(f"{link}/users?id={user_id}")
+    name = user_info.json()[0]["username"]
+    todos = todos.json()
     result = {}
-    for user in users:
-        todos = requests.get(
-            f"{link}/todos?userId={user["id"]}"
+    result[user_id] = []
+    for todo in todos:
+        result[user_id].append(
+            {
+                "task": todo["title"],
+                "completed": todo["completed"],
+                "username": name,
+            }
         )
-        todos = todos.json()
-        result[user["id"]] = []
-        for todo in todos:
-            result[user["id"]].append(
-                {
-                    "username": user["username"],
-                    "task": todo["title"],
-                    "completed": todo["completed"],
-                }
-            )
-    with open("todo_all_employees.json", "w") as result_file:
+    with open(f"{user_id}.json", "w") as result_file:
         json.dump(result, result_file)
